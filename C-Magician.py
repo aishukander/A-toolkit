eedb7a953636d395b96f54cf2af6e18680428ce3
aishukander <aishukander@gmail.com>
2023-06-03 14:56:31 +0800

    完善無效選項

--- C-Magician.py
+++ C-Magician.py
@@ -48,23 +48,39 @@ def delete_prefetch_files():
 def clear_recycle_bin():
     os.system('PowerShell.exe -Command Clear-RecycleBin -Force')
 
-print('請選擇要執行哪些功能：')
-print('1. 刪除Temp中的檔案')
-print('2. 刪除Prefetch中的檔案')
-print('3. 清空回收桶')
-print('4. 全選')
+while True:
+    print('請選擇要執行哪些功能：')
+    print('1. 刪除Temp中的檔案')
+    print('2. 刪除Prefetch中的檔案')
+    print('3. 清空回收桶')
+    print('4. 全選')
 
-choice = input('請輸入您的選擇（1/2/3/4）：')
+    choice = input('請輸入您的選擇（1/2/3/4）：')
 
-if choice == '1':
-    delete_temp_files()
-elif choice == '2':
-    delete_prefetch_files()
-elif choice == '3':
-    clear_recycle_bin()
-elif choice == '4':
-    delete_temp_files(),delete_prefetch_files(),clear_recycle_bin()
-else:
-    print('無效的選擇。')
-    input('按任意鍵繼續...')
-    os.system('cls' if os.name == 'nt' else 'clear')
+    if choice == '1':
+        delete_temp_files()
+        break
+    elif choice == '2':
+        delete_prefetch_files()
+        break
+    elif choice == '3':
+        clear_recycle_bin()
+        break
+    elif choice == '4':
+        delete_temp_files()
+        delete_prefetch_files()
+        clear_recycle_bin()
+        break
+    else:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("********************************************************")
+        print("*                                                      *")
+        print("*                                                      *")
+        print("*                                                      *")
+        print("*                       無效選項                       *")
+        print("*                                                      *")
+        print("*                                                      *")
+        print("*                                                      *")
+        print("********************************************************")
+        input('按任意鍵繼續...')
+        os.system('cls' if os.name == 'nt' else 'clear')
