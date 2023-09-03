40f28da968428d604b133005263d9f65bc3a6270
aishukander <aishukander@gmail.com>
2023-09-03 17:27:45 +0800

    新增Word-Changer工具

---
+++ Word-Changer/Word-Changer.py
@@ -0,0 +1,35 @@
+#導入模組
+import tkinter
+import re
+import os
+#導入filedialog函數
+from tkinter.filedialog import askopenfilename
+
+#隱藏根視窗
+tkinter.Tk ().withdraw ()
+
+#打開檔案選擇對話框，並將所選檔案的路徑儲存為filename變數
+filename = askopenfilename ()
+
+#讀取txt文本的內容，並將其儲存為text變數
+with open(filename, "r", encoding="utf-8") as f:
+    text = f.read()
+
+#詢問使用者要替換的單字，並將其儲存為old變數
+old = input("請輸入您要替換的單字：")
+#詢問使用者要替換成什麼單字，並將其儲存為new變數
+new = input("請輸入您要替換成什麼單字：")
+
+#替換文本中的單字，並將修改後的文本儲存為new_text變數
+new_text = re.sub(old, new, text)
+#分割檔案名稱和副檔名，並將其儲存為pre和ext變數
+pre, ext = os.path.splitext(filename)
+#使用+運算符來連接新的檔案名稱和副檔名，並將其儲存為new_filename變數
+new_filename = pre + " revise" + ext
+
+#寫入修改後的文本到一個新的txt檔案，並將其儲存到跟程式同個資料夾
+with open(new_filename, "w", encoding="utf-8") as f:
+    f.write(new_text)
+
+#輸出成功訊息
+print("文本修改完成，已儲存到" + new_filename)
